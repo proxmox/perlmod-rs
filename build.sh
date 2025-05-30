3771bf02e398d414525284cbb3142faddde604af
Thomas Lamprecht <t.lamprecht@proxmox.com>
2025-05-30 19:03:38 +0200

    buildsystem: support dsc and over other improvements from proxmox workspace
    Fully copied over the build.sh script from the proxmox workspace crate
    repo (commit 956cd368) and added the dsc targets in the Makefile.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- build.sh
+++ build.sh
@@ -7,13 +7,33 @@ export RUSTC=/usr/bin/rustc
 
 CRATE=$1
 BUILDCMD=${BUILDCMD:-"dpkg-buildpackage -b -uc -us"}
+BUILDDIR="${BUILDDIR:-"build"}"
 
-mkdir -p build
-echo system >build/rust-toolchain
-rm -rf "build/${CRATE}"
+mkdir -p "${BUILDDIR}"
+echo system >"${BUILDDIR}"/rust-toolchain
+rm -rf ""${BUILDDIR}"/${CRATE}"
 
-rm -f "${CRATE}/debian/control"
-debcargo package --config "$PWD/${CRATE}/debian/debcargo.toml" --changelog-ready --no-overlay-write-back --directory "$PWD/build/${CRATE}" "${CRATE}" "$(dpkg-parsechangelog -l "${CRATE}/debian/changelog" -SVersion | sed -e 's/-.*//')"
-cp "build/${CRATE}/debian/control" "${CRATE}/debian/control"
-cd "build/${CRATE}"
+CONTROL="$PWD/${CRATE}/debian/control"
+
+if [ -e "$CONTROL" ]; then
+    # check but only warn, debcargo fails anyway if crates are missing
+    dpkg-checkbuilddeps $PWD/${CRATE}/debian/control || true
+    [ "x$NOCONTROL" = 'x' ] && rm -f "$PWD/${CRATE}/debian/control"
+fi
+
+debcargo package \
+    --config "$PWD/${CRATE}/debian/debcargo.toml" \
+    --changelog-ready \
+    --no-overlay-write-back \
+    --directory "$PWD/"${BUILDDIR}"/${CRATE}" \
+    "${CRATE}" \
+    "$(dpkg-parsechangelog -l "${CRATE}/debian/changelog" -SVersion | sed -e 's/-.*//')"
+
+cd ""${BUILDDIR}"/${CRATE}"
+rm -f debian/source/format.debcargo.hint
 ${BUILDCMD}
+
+# needs all crates build-dependencies, which can be more than what debcargo assembles.
+[ "x$NOTEST" = "x" ] && $CARGO test --all-features --all-targets
+
+[ "x$NOCONTROL" = "x" ] && cp debian/control "$CONTROL"
